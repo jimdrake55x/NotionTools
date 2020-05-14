9b7ef7f59ddcfdec23e72fa992bb46563aa97e8e
Jim Drake <jimdrake55x@gmail.com>
2020-05-14 13:34:52 -0500

    JTN-1] [ADD] Added config file and examples

--- JiraToNotion/notion_builder/notion_builder.py
+++ JiraToNotion/notion_builder/notion_builder.py
@@ -1,4 +1,7 @@
 from notion.client import NotionClient
+from notion.block import TodoBlock
+import json
+import os
 
 
 def say_hello():
@@ -6,14 +9,39 @@ def say_hello():
 
 
 def test_insert_page():
+
+    notion_config_data = load_notion_configs()
+    token = notion_config_data['token']
+    base_page = notion_config_data['base_page']
+
     # client
-    client = NotionClient(
-        token_v2="Token Goes here)
+    client = NotionClient(token_v2=token)
 
     # Page to edit
-    page = client.get_block(
-        "https://www.notion.so/Test-Page-please-ignore-eb59a27d75394487a048e3fa05d3751a")
+    page = client.get_block(base_page)
 
-    print("The old title is:", page.title)
+    newCheckBox = page.children.add_new(
+        TodoBlock, title="Something to get done")
+    newCheckBox.checked = True
+
+    print("The current title is:", page.title)
 
     page.title = "The title is changed, GET USED TO IT"
+
+
+def load_notion_configs():
+    try:
+        config_path = os.path.abspath(
+            './JiraToNotion/configs/notion_config.json')
+        notion_data = get_notion_data(config_path)
+    except:
+        print("Unable to locate the notion_config.json file. do you ahve one created in the root of the directory?")
+
+    return notion_data
+
+
+def get_notion_data(config_file):
+    with open(config_file) as config:
+        data = json.load(config)
+
+    return data
