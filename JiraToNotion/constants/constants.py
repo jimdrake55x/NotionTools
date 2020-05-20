2a34ea9ab71d81047b895bc9afa7951b57e21fd5
Jim Drake <jimdrake55x@gmail.com>
2020-05-20 08:40:48 -0500

    [NOTICK] [ADD] Jira builder runs on constants now

--- JiraToNotion/constants/constants.py
+++ JiraToNotion/constants/constants.py
@@ -1,2 +1,18 @@
+
+# Jira request constants
 JIRA_SPRINTS_URL = 'https://hudl-jira.atlassian.net/rest/agile/1.0/board/{pn}/sprint'
 JIRA_SPRINTS_URL_PROJECT_REPLACE = '{pn}'
+
+# Folder Path Constants
+FOLDER_DATA = './JiraToNotion/data/'
+FOLDER_DATA_FILE_REPLACE = '{f}'
+FOLDER_DATA_FILE = FOLDER_DATA + FOLDER_DATA_FILE_REPLACE
+
+FOLDER_CONFIG = './JiraToNotion/configs/'
+FOLDER_CONFIG_FILE_REPLACE = '{cf}'
+FOLDER_CONFIG_FILE = FOLDER_CONFIG + FOLDER_CONFIG_FILE_REPLACE
+
+# File Constants
+FILE_SPRINT_DATA = 'data_sprint.json'
+FILE_JIRA_CONFIG = 'jira_config.json'
+FILE_NOTION_CONFIG = 'notion_config.json'
--- JiraToNotion/jira_builder/jira_builder.py
+++ JiraToNotion/jira_builder/jira_builder.py
@@ -1,6 +1,6 @@
 from config_builder.config_builder import get_file_data
 from models.sprint import Sprint
-from constants.constants import JIRA_SPRINTS_URL, JIRA_SPRINTS_URL_PROJECT_REPLACE
+from constants.constants import JIRA_SPRINTS_URL, JIRA_SPRINTS_URL_PROJECT_REPLACE, FOLDER_DATA, FOLDER_DATA_FILE, FOLDER_DATA_FILE_REPLACE, FILE_SPRINT_DATA, FOLDER_CONFIG_FILE, FOLDER_CONFIG_FILE_REPLACE, FILE_JIRA_CONFIG
 import json
 import os
 import requests
@@ -20,11 +20,13 @@ def make_sprint_requests():
         response = requests.get(api, auth=(username, token)).json()
 
         # Create a data directory if it doesnt exist
-        if not os.path.exists('./JiraToNotion/data/'):
-            os.makedirs('./JiraToNotion/data/')
+        if not os.path.exists(FOLDER_DATA):
+            os.makedirs(FOLDER_DATA)
 
         # Write the sprint data to the data directory
-        with open('./JiraToNotion/data/data_sprint.json', 'w') as outfile:
+        write_file = FOLDER_DATA_FILE.replace(
+            FOLDER_DATA_FILE_REPLACE, FILE_SPRINT_DATA)
+        with open(write_file, 'w') as outfile:
             json.dump(response, outfile)
 
     except Exception as e:
@@ -36,8 +38,9 @@ def make_sprint_requests():
 
 def get_jira_config_data():
     try:
-        config_path = os.path.abspath(
-            './JiraToNotion/configs/jira_config.json')
+        jira_config = FOLDER_CONFIG_FILE.replace(
+            FOLDER_CONFIG_FILE_REPLACE, FILE_JIRA_CONFIG)
+        config_path = os.path.abspath(jira_config)
         jira_data = get_file_data(config_path)
     except:
         print("Unable to locate the jira_config.json file. do you ahve one created in the root of the directory?")
